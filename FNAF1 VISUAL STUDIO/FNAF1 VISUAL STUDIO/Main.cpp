57c702ade07b4f277bcdcbe6dfed0511ccfbdfdd
Egory <egoryfilenk@gmail.com>
2025-02-04 17:40:15 -0500

    i drew the map. door closing mechanism NOT complete

---
+++ FNAF1 VISUAL STUDIO/FNAF1 VISUAL STUDIO/Main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "Map.h"
+
+std::string action;
+void Actions(const std::string newaction)
+{
+	if (newaction == "close left door" || "Close right door")
+	{
+		leftdoor = "|||";
+	}
+}
+int main()
+{
+	printMap();
+	std::cin >> action;
+	Actions(action);
+
+}
---
+++ FNAF1 VISUAL STUDIO/FNAF1 VISUAL STUDIO/Map.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+bool doorclosedLEFT = false;
+bool doorclosedRIGHT = false;
+std::string leftdoor = "   ";
+std::string rightdoor = "   ";
+void printMap()
+{
+	
+	std::cout << "               oCAM1A----------o               " << '\n';
+	std::cout << "               |     STAGE     |               " << '\n';
+	std::cout << "         o-----o---------------o-----o         " << '\n';
+	std::cout << "    o--o-o  CAM1B                    o---CAM7-o" << '\n';
+	std::cout << "CAM5|                                         |" << '\n';
+	std::cout << "    o--o-o                           o--o  o--o" << '\n';
+	std::cout << "         |CAM1C                      |  |     |" << '\n';
+	std::cout << "         o---o    o---------o    o---o  o--o--o" << '\n';
+	std::cout << "             |    |         |    | o---o       " << '\n';
+	std::cout << "             |CAM2A         |CAM4A |   |       " << '\n';
+	std::cout << "      o---o  |    |         |    | |   |CAM6   " << '\n';
+	std::cout << "  CAM3|   o--o    o-o-----o-o    o-o   |       " << '\n';
+	std::cout << "      |           " << leftdoor << " YOU " << rightdoor << "          |       " << '\n';
+	std::cout << "      o---o--o    o-o-----o-o    o-o---o       " << '\n';
+	std::cout << "             |    |         |    |             " << '\n';
+	std::cout << "             |CAM2B         |CAM4B             " << '\n';
+}
+void updatemap(std::string door, std::string state)
+{
+	
+}
