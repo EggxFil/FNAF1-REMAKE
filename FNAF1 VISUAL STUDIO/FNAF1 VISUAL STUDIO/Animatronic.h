8bb9e376703fb9d6ede0f2525f61d643c1a7857b
Egory <egoryfilenk@gmail.com>
2025-02-11 21:47:33 -0500

    Linker errors solved. TODO: patch memory leak

--- FNAF1 VISUAL STUDIO/FNAF1 VISUAL STUDIO/Animatronic.h
+++ FNAF1 VISUAL STUDIO/FNAF1 VISUAL STUDIO/Animatronic.h
@@ -4,20 +4,45 @@
 class animatronicStages
 {
 public:
-	int* harray;
-	int arraybuffer;
-	animatronicStages();
-	
-	int returnarray(const int& index);
-	
-	~animatronicStages();
-	
+	int* harray = nullptr;
+	int arraybuffer = 0;
+
+	animatronicStages()
+	{
+
+		harray = new int[5];
+
+		for (int i = 0; 5 > i; i++)
+		{
+			arraybuffer++;
+			harray[i] = arraybuffer;
+			Print(harray[i]);
+		}
+	}
+	int returnarray(const int& index)
+	{
+		return harray[index];
+	}
+
+	~animatronicStages()
+	{
+		delete harray;
+	}
 
 };
 class animatronic
 {
 public:
 	//int* stagebuffer;
-	int stage;
-	animatronic(std::string name, int stages);
+	int stage = 0;
+	animatronic(std::string name, int stages)
+	{
+		animatronicStages* newstage = new animatronicStages();
+		stage = newstage->returnarray(0);
+		Print(stage);
+	}
+	~animatronic()
+	{
+		delete newstage;
+	}
 };
